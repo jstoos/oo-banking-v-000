65148dd3030b3c6ec1d7d31c983ef01006c32790
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-14 16:17:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
       # @@all << self
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
+      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
