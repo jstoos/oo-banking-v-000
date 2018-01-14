1cf6ff443d1415ebabb5b25852bcca8ff2f9e422
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-14 16:35:30 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     # binding.pry
-    if @sender.valid? && duplicate? == false
+    if valid? && duplicate? == false
       @@all << self
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
