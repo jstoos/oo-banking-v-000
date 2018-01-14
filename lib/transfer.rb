231cf08319405e2502f89730c9ff7d3eeb87fc56
Jennifer Stoos <jstoos@bitlaw.com>
2018-01-14 15:57:40 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,17 +1,27 @@
 class Transfer
   attr_accessor :sender, :receiver, :status, :amount
 
+  @@all = []
+
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
     @amount = amount
+    @@all << self
   end
 
   def valid?
     @sender.valid? && @receiver.valid?
   end
 
+  def execute_transaction
+
+  end
+
+  def duplicate?
+    @@all.include?(self)
+  end
 
 
 
